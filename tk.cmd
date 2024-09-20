b709bb7105317711bdf8f3bca63971b6fb4f0fd2
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-09-20 08:55:14 -0700

    Taskfile cleanup
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- tk.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-goto:$main
-
-:$main
-    setlocal EnableDelayedExpansion
-    set "PATH=%PATH%;%USERPROFILE%\scoop\apps\ruby\current\bin;%USERPROFILE%\scoop\persist\ruby\gems\bin"
-    echo ##[cmd] task %*
-    task %*
-endlocal & exit /b %ERRORLEVEL%
